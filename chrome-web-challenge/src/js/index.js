d91ed6118fb62696c4080fa400d5be18358de42c
신건희 <godgh@emforce.co.kr>
2024-05-31 18:44:13 +0900

    🔨 add todo

--- chrome-web-challenge/src/js/index.js
+++ chrome-web-challenge/src/js/index.js
@@ -8,6 +8,8 @@ const imageArray = [
   'url("./src/asset/image/6.jpg")',
   'url("./src/asset/image/7.jpg")',
 ];
+
+// 날짜
 function getDate() {
   const currentTime = new Date();
   const calendar = document.getElementById('day');
@@ -38,18 +40,18 @@ getDate();
 setInterval(getClock, 1000);
 getBackgroundImg();
 
+// 로그인
 const loginContainer = document.getElementById('login-container');
 const loginButton = document.getElementById('login-button');
 const loginName = document.getElementById('login-name');
-
+const userContainer = document.getElementById('user-container');
 function checkName() {
-  const name = window.localStorage.getItem('loginName');
-  if (name == null) {
-    console.log('없음');
-  } else {
+  const name = localStorage.getItem('loginName');
+  if (name != null) {
     loginContainer.classList.add('hidden');
     const userName = document.getElementById('user-name');
     userName.innerText = `Welcome ${name}!`;
+    userContainer.style.display = 'block';
   }
 }
 
@@ -57,7 +59,96 @@ checkName();
 
 function onClickLogin(event) {
   event.preventDefault();
-  window.localStorage.setItem('loginName', loginName.value);
+  localStorage.setItem('loginName', loginName.value);
   checkName();
 }
+
 loginButton.addEventListener('click', onClickLogin);
+
+// todo-list
+let todoArray = [];
+const TODOS_KEY = 'todos';
+const todoIcon = document.getElementById('todo-icon');
+const exitIcon = document.getElementById('exit-icon');
+const todoContainer = document.getElementById('todo-container');
+const addBtn = document.getElementById('todo-add-button');
+const listContainer = document.getElementById('list-container');
+
+function onClickTodo() {
+  todoIcon.classList.add('animate-slideout');
+  todoIcon.addEventListener('animationend', function () {
+    todoIcon.style.display = 'none';
+  });
+  todoContainer.classList.add('animate-slidein');
+  todoContainer.addEventListener('animationend', function () {
+    todoContainer.style.right = 0;
+  });
+}
+
+function onClickExit() {
+  todoContainer.classList.remove('animate-slidein');
+  todoIcon.classList.remove('animate-slideout');
+  todoContainer.classList.add('animate-slideout');
+  todoIcon.style.display = 'block';
+  todoContainer.addEventListener('animationend', function () {
+    todoContainer.style.right = '-50%';
+  });
+
+  todoIcon.classList.add('animate-slidein');
+  todoIcon.addEventListener('animationend', function () {
+    todoIcon.style.display = 'block';
+    todoIcon.classList.remove('animate-slidein');
+  });
+}
+
+function onClickAdd(event) {
+  event.preventDefault();
+  const todoText = document.getElementById('todo');
+  if (todoText.value.length != 0) {
+    const newObj = {
+      text: todoText.value,
+      id: Date.now(),
+    };
+    todoArray.push(newObj);
+    paintTodo(newObj);
+    saveTodos();
+    todoText.value = '';
+  }
+}
+
+function paintTodo(todoObj) {
+  const li = document.createElement('li');
+  li.id = todoObj.id;
+  const span = document.createElement('span');
+  span.innerText = todoObj.text;
+  const button = document.createElement('button');
+  button.innerText = '❌';
+  button.addEventListener('click', deleteTodo);
+  li.appendChild(span);
+  li.appendChild(button);
+  listContainer.appendChild(li);
+}
+
+function saveTodos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(todoArray));
+}
+
+function deleteTodo(event) {
+  event.preventDefault();
+  const li = event.target.parentElement;
+  li.remove();
+  todoArray = todoArray.filter(todo => todo.id !== parseInt(li.id));
+  saveTodos();
+}
+
+const savedTodos = localStorage.getItem(TODOS_KEY);
+
+if (savedTodos) {
+  const parsedTodos = JSON.parse(savedTodos);
+  parsedTodos.forEach(item => paintTodo(item));
+  todoArray = parsedTodos;
+}
+
+addBtn.addEventListener('click', onClickAdd);
+todoIcon.addEventListener('click', onClickTodo);
+exitIcon.addEventListener('click', onClickExit);
